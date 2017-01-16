d361d32da0ba8737941a38b8d0cbcf0b689060c1
Cristian Cristea <cristiancristea.1987@gmail.com>
2017-01-16 16:08:51 +0100

    Remove comments

--- scripts/search.js
+++ scripts/search.js
@@ -1,3 +1,4 @@
+var cities = [];
 var substringMatcher = function(strs) {
   return function findMatches(q, cb) {
     var matches, substringRegex;
@@ -20,8 +21,6 @@ var substringMatcher = function(strs) {
   };
 };
 
-var cities = [];
-
 $.getJSON('city.list.json', function(response) {
     $.each(response, function(index, city) {
       cities.push(city.name);
@@ -30,17 +29,6 @@ $.getJSON('city.list.json', function(response) {
     var err = textStatus + ", " + error;
     console.log( "Request Failed: " + err );
   });
-
-// var states = ['London', 'Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
-//   'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
-//   'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
-//   'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
-//   'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
-//   'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
-//   'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
-//   'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
-//   'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
-// ];
 
 $('#the-basics .typeahead').typeahead({
   hint: true,
